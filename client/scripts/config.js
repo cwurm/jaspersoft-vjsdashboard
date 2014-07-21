0eec819190672404213e6d2c09d7d374befcdeca
Christoph Wurm <wurm.ch@gmail.com>
2014-07-21 16:56:01 +0200

    Initial version for Github.

---
+++ client/scripts/config.js
@@ -0,0 +1,35 @@
+var config = {
+
+	// URL of the JRS
+	jrsUrl: "http://localhost:8080/jasperserver-pro",
+
+	// JRS login username
+	jrsUsername: "jasperadmin",
+
+	// JRS login password
+	jrsPassword: "jasperadmin",
+
+	// JRS login organization
+	jrsOrganization: "organization_1",
+
+	// The folder on JRS that will be searched for reports to add.
+	folderUri: "/public/Dashboard_Reports", 
+
+	// Indicates whether JRS should be searched for reports only
+	// in the given folder (see option folderUri above) or also
+	// in all sub-folders.
+	recursive: false,
+
+	// URL of the Dashboardsaver servlet
+	dashboardSaver: "http://127.0.0.1:8080/dashboardsaver",
+
+	// Initial height of the application, in px
+	defaultApplicationHeight: 800,
+
+	// Initial width of reports when added to a dashboard, in px
+	defaultReportWidth: 400,
+
+	// Initial height of reports when added to a dashboard, in px
+	defaultReportHeight: 300
+}
+
